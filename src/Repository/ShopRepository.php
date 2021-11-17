10910bd6a4e69cc2cbe7d00fbb51f291c76d05e1
RIEBER BAPTISTE p1908624 <baptiste.rieber@etu.univ-lyon1.fr>
2021-11-17 13:43:00 +0100

    changement de route de base

--- src/Repository/ShopRepository.php
+++ src/Repository/ShopRepository.php
@@ -22,19 +22,19 @@ class ShopRepository extends ServiceEntityRepository
     // /**
     //  * @return Shop[] Returns an array of Shop objects
     //  */
-    /*
-    public function findByExampleField($value)
+    
+    public function findAllShops()
     {
         return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
+            // ->andWhere('s.exampleField = :val')
+            // ->setParameter('val', $value)
+            // ->orderBy('s.id', 'ASC')
+            // ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
+    
 
     /*
     public function findOneBySomeField($value): ?Shop
