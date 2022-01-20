e504f1ef2eaffc56ae86a8f57c0e765fffbcbd9f
Galdom <52220903+Galdom@users.noreply.github.com>
2022-01-20 09:10:09 +0100

    Ajout vue des messages

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -5,17 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Message;
 
-/**
- * @Route("/message", name="message")
- */
 class MessageController extends AbstractController
 {
 
+    /**
+     * @Route("/message", name="message")
+     */
     public function index(): Response
     {
+        $manager = $this->getDoctrine()->getManager();
+
+        $messages = $manager->getRepository(Message::class)->findAll();
+
         return $this->render('message/index.html.twig', [
             'controller_name' => 'MessageController',
+            'messages' => $messages
         ]);
     }
 }
